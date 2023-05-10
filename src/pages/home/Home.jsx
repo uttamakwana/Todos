9a65f1efb161759ac69ab037582bd0b07eece483
Uttam Makwana <uttamakwana4503@gmail.com>
2023-05-10 16:52:27 +0530

    Done with header and going further

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import Todos from '../../components/todos/Todos'
 
 const Home = () => {
   return (
     <>
         <Header />
+        <Todos />
     </>
   )
 }
