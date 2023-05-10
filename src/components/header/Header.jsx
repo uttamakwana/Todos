07036a4c03ec0792a44e6c742021a8fe381751e6
Uttam Makwana <uttamakwana4503@gmail.com>
2023-05-10 16:12:51 +0530

    Made header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <>
+        <header className="header">
+            <h1 id="header-logo-text">Todos</h1>
+        </header>
+    </>
+  )
+}
+
+export default Header
